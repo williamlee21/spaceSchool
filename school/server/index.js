02dc848fb0e0727a4a8aec005a709c14713f4cda
William Lee <williamlee021@gmail.com>
2018-07-31 14:48:18 -0400

    added: graphql routes

--- school/server/index.js
+++ school/server/index.js
@@ -1,7 +1,52 @@
 const express = require("express")
 const app = express()
 const {db} = require('./db')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
+const {Student, School} = require('./db/models')
 
+
+var schema = buildSchema(`
+    type Query  {
+        getStudent(id:Int!): [Student]
+        getSchool: School
+    }
+
+    type Student {
+        id:ID
+        name: String
+        grade: String
+        schoolId: ID
+    }    
+    type School {
+        id: ID
+        name:String
+        description: String
+        updatedAt: String
+        student: Student
+    }
+`)
+ var root = {
+    getStudent: (args) => {
+       if (args.id) {
+           return Student.findAll({where:{schoolId: args.id}})
+       } else {
+        return Student.findAll()
+       }
+
+
+    },
+    getSchool: () => {
+        return School.findOne({where:{id: 1}})
+    }
+     
+ }
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+}))
 app.get('/', (req, res) => res.send('Hello World'))
 
 db.sync()
